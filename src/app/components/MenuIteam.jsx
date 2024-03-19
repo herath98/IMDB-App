e38f52df351afa0e6b686d3b8da698343754919c
harsha <harshaudayanga401@gmail.com>
2024-03-19 15:49:17 +0530

    darkmood

--- src/app/components/MenuIteam.jsx
+++ src/app/components/MenuIteam.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function MenuIteam({title, address, Icon}) {
   return (
-    <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600'>
+    <Link href={address} className='mx-4 lg:mx-6 '>
         <Icon className="text-2xl sm:hidden"/>
         <p className="uppercase hidden sm:inline text-sm">{title}</p>
     </Link>
   )
-}
+};
