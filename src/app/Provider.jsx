129b752727cb7bdd302b6b809047abb2545bb768
harsha <harshaudayanga401@gmail.com>
2024-03-19 18:53:13 +0530

    about page

--- src/app/Provider.jsx
+++ src/app/Provider.jsx
@@ -6,8 +6,8 @@ export default function Provider({ children }) {
   return (
     <ThemeProvider>
       <div defaultTheme='system' attribute='class'
-        className="light:text-gray-400 dark:text-gray-200
-         dark:bg-gray-700 min-h-screen select-none transition-colors
+        className="light:text-gray-400 light:text-blue-200
+         light:bg-gray-700 min-h-screen select-none transition-colors
           duration-300 ">
         {children}
       </div>
---
+++ src/app/about/page.jsx
@@ -0,0 +1,34 @@
+// pages/about.jsx
+
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto p-3 sm:px-6 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About Us</h1>
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut justo
+        eget libero sodales congue. Sed et consectetur mauris. Proin euismod
+        odio nec purus rhoncus, et suscipit lacus vestibulum. Integer
+        tincidunt, ex eu finibus sollicitudin, lorem nisi convallis mauris, non
+        consequat dui nisi nec sem. Donec bibendum felis sed neque ultricies,
+        at dictum lorem facilisis.
+      </p>
+      <p className="mb-4">
+        Nullam varius ullamcorper justo, non fermentum lorem vestibulum vel.
+        Nullam vel tempor lacus. In hac habitasse platea dictumst. Integer
+        eget sollicitudin eros. Integer auctor commodo ligula, eget molestie
+        metus dapibus eu. Proin ut sapien eu orci mollis gravida. Suspendisse
+        potenti.
+      </p>
+      <p>
+        Vivamus non libero eu magna ultrices consequat. Sed maximus aliquet
+        tortor, in eleifend magna sodales eget. Integer auctor, dolor vel
+        aliquet sollicitudin, velit velit accumsan mauris, eget molestie dui
+        orci a mi.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
